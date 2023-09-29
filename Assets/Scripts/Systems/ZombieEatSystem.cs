b15c7b5f8e43d81bee835a32eeaaa772e2fa5ca1
ofgonen-adastec <ofgonen@gmail.com>
2023-09-29 14:53:19 +0300

    feat: implement zombie eat system

---
+++ Assets/Scripts/Systems/ZombieEatSystem.cs
@@ -0,0 +1,43 @@
+using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+namespace TMG.Zombies
+{
+    [BurstCompile]
+    [UpdateAfter(typeof(ZombieWalkSystem))]
+    public partial struct ZombieEatSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            new ZombieEatJob
+            {
+                DeltaTime = deltaTime
+            }.ScheduleParallel();
+        }
+    }    
+    
+    [BurstCompile]
+    public partial struct ZombieEatJob : IJobEntity
+    {
+        public float DeltaTime;
+
+        [BurstCompile]
+        private void Execute(ZombieEatAspect zombie)
+        {
+            zombie.Eat(DeltaTime);
+        }
+    }
+}
