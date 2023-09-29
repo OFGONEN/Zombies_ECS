bd6cc066828b07767a0ef3c1923c714aacdb2101
ofgonen-adastec <ofgonen@gmail.com>
2023-09-29 14:56:43 +0300

    feat: brain health component

--- Assets/Scripts/AuthoringAndMono/BrainMono.cs
+++ Assets/Scripts/AuthoringAndMono/BrainMono.cs
@@ -5,6 +5,7 @@ namespace TMG.Zombies
 {
     public class BrainMono : MonoBehaviour
     {
+        public float BrainHealth;
     }
 
     public class BrainBaker : Baker<BrainMono>
@@ -12,6 +13,11 @@ namespace TMG.Zombies
         public override void Bake(BrainMono authoring)
         {
             AddComponent<BrainTag>();
+            AddComponent(new BrainHealth
+            {
+                Value = authoring.BrainHealth,
+                Max = authoring.BrainHealth
+            });
         }
     }
 }
